76d1dda7117b62a9ffa70858fad79491ccaab4ce
JoseB1342 <bellojoseabimael@gmail.com>
2026-07-24 08:50:11 -0600

    correccion

--- semana2/eval1/test_iot.py
+++ semana2/eval1/test_iot.py
@@ -3,10 +3,8 @@ from datetime import datetime
 from semana2.eval1.iot import SensorReading
 
 def test_sensor_reading_creation() -> None:
-    # Given / When
     reading = SensorReading(sensor_id="SENS-01", temperature=25.0, humidity=50.0)
     
-    # Then
     assert reading.sensor_id == "SENS-01"
     assert reading.temperature == 25.0
     assert reading.humidity == 50.0
