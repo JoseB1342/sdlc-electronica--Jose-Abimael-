8c843172445b9420789f1dce7deb7ac6780a95d8
Jose_Abimael <belloalarconjoseabimael@gmail.com>
2026-08-19 12:19:59 -0600

    test: crear tablas de base de datos antes de ejecutar pytest

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,11 @@
 import uuid
+from app.db import Base, engine
+
+from app.models.sensor import SensorModel
+from app.models.reading import ReadingModel
+from app.models.alert import AlertModel
+
+Base.metadata.create_all(bind=engine)
 
 from fastapi.testclient import TestClient
 
