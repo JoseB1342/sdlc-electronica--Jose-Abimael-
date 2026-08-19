419c0d4e9b649529755f0b7927830efe508d2632
Jose_Abimael <belloalarconjoseabimael@gmail.com>
2026-08-19 11:49:40 -0600

    fix: ordenar imports de alembic manualmente

--- migrations/versions/2ca3981c9dee_agregar_tabla_alertas.py
+++ migrations/versions/2ca3981c9dee_agregar_tabla_alertas.py
@@ -8,7 +8,6 @@ Create Date: 2026-08-17 17:54:03.590972
 from collections.abc import Sequence
 
 import sqlalchemy as sa
-
 from alembic import op
 
 # revision identifiers, used by Alembic.
--- migrations/versions/c57f2e7b3ded_crear_tablas_iniciales.py
+++ migrations/versions/c57f2e7b3ded_crear_tablas_iniciales.py
@@ -8,7 +8,6 @@ Create Date: 2026-08-17 17:40:46.542308
 from collections.abc import Sequence
 
 import sqlalchemy as sa
-
 from alembic import op
 
 # revision identifiers, used by Alembic.
